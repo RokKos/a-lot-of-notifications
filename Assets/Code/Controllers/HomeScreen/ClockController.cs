7a859db928f4b72d90634cf502fec4e5ef835533
RokKos <rocki5555@gmail.com>
2020-03-14 14:55:30 +0100

    TextMeshPro import and Clock implemented

---
+++ Assets/Code/Controllers/HomeScreen/ClockController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+public class ClockController : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI clockText = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        var hours = time.Hour;
+        var minute = time.Minute;
+        clockText.SetText(hours.ToString("D2") + ":" + minute.ToString("D2"));
+    }
+}
