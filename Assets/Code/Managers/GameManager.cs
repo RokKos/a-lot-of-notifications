cf59a7a0367604070f748c1f38f1c97e2bd1edb9
RokKos <rocki5555@gmail.com>
2020-03-14 09:44:15 +0100

    Switching between scene

--- Assets/Code/Managers/GameManager.cs
+++ Assets/Code/Managers/GameManager.cs
@@ -1,12 +1,23 @@
-﻿using System.Collections;
+﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Assertions;
+
+public enum EventType
+{
+    Back = 0,
+    ToHomeScreen = 1,
+    ToEmailSortingScreen = 2,
+    ToClickerScreen = 3,
+    ToCountingScreen = 4
+}
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<BaseScreenController> baseScreenControllers = new List<BaseScreenController>();
-    
+
+    private BaseScreenController curr_screen_controller = null;
+
+
     void Start()
     {
         foreach (var baseScreenController in baseScreenControllers)
@@ -14,17 +25,70 @@ public class GameManager : MonoBehaviour
             baseScreenController.OnScreenDisable();
         }
 
-        var homeScreen = FindScreen(ScreenTypes.HomeScreen);
-        homeScreen.OnScreenEnable();
+        curr_screen_controller = FindScreen(ScreenTypes.HomeScreen);
+        curr_screen_controller.OnScreenEnable();
     }
 
-    
+
     void Update()
     {
-        
     }
-    
-    
+
+    public void SendEvent(string eventTypeStr)
+    {
+        EventType eventType = ConvertFromStr(eventTypeStr);
+
+        curr_screen_controller.OnScreenExit();
+        curr_screen_controller.OnScreenDisable();
+
+        switch (eventType)
+        {
+            case EventType.Back:
+            {
+                Debug.Log("Back Event Send");
+                break;
+            }
+
+            case EventType.ToHomeScreen:
+            {
+                curr_screen_controller = FindScreen(ScreenTypes.HomeScreen);
+                Debug.Log("ToHomeScreen Event Send");
+                break;
+            }
+
+            case EventType.ToEmailSortingScreen:
+            {
+                curr_screen_controller = FindScreen(ScreenTypes.EmailSortingScreen);
+                Debug.Log("ToEmailSortingScreen Event Send");
+                break;
+            }
+
+            case EventType.ToClickerScreen:
+            {
+                curr_screen_controller = FindScreen(ScreenTypes.ClickerScreen);
+                Debug.Log("ToClickerScreen Event Send");
+                break;
+            }
+
+            case EventType.ToCountingScreen:
+            {
+                curr_screen_controller = FindScreen(ScreenTypes.CountingScreen);
+                Debug.Log("ToCountingScreen Event Send");
+                break;
+            }
+
+            default:
+            {
+                Debug.Log("Event was not handled");
+                break;
+            }
+        }
+        
+        curr_screen_controller.OnScreenEnable();
+        curr_screen_controller.OnScreenEnter();
+    }
+
+
     // HELPER FUNCTIONS
 
     BaseScreenController FindScreen(ScreenTypes type)
@@ -39,5 +103,27 @@ public class GameManager : MonoBehaviour
 
         Debug.Log("No base screen type found");
         return baseScreenControllers[0];
+    }
+
+    readonly Dictionary<string, EventType> _stringToEventEnum = new Dictionary<string, EventType>()
+    {
+        {"Back", EventType.Back},
+        {"ToHomeScreen", EventType.ToHomeScreen},
+        {"ToEmailSortingScreen", EventType.ToEmailSortingScreen},
+        {"ToClickerScreen", EventType.ToClickerScreen},
+        {"ToCountingScreen", EventType.ToClickerScreen}
+    };
+
+    private EventType ConvertFromStr(string str)
+    {
+        EventType conv;
+
+        if (_stringToEventEnum.TryGetValue(str, out conv))
+        {
+            return conv;
+        }
+
+        Debug.Log("WRONG STRING FOR EVENT");
+        return EventType.ToHomeScreen;
     }
 }
