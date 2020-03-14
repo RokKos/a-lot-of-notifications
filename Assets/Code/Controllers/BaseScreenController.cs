a9aae91069049ce95f2c7ff7744f68346cac8cf9
RokKos <rocki5555@gmail.com>
2020-03-14 09:10:22 +0100

    Base arhitecture that we will build on

---
+++ Assets/Code/Controllers/BaseScreenController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public enum ScreenTypes
+{
+    HomeScreen = 0,
+    EmailSortingScreen = 1,
+    ClickerScreen = 2,
+    CountingScreen = 3
+}
+
+public class BaseScreenController : MonoBehaviour
+{
+    [SerializeField] private ScreenTypes screenType = ScreenTypes.HomeScreen;
+    
+    public ScreenTypes GetScreenType() {
+        return screenType;
+    }
+
+
+    public virtual void OnScreenEnter()
+    {
+        
+    }
+    
+    public virtual void OnScreenExit()
+    {
+        
+    }
+    
+    public virtual void OnScreenUpdate()
+    {
+        
+    }
+    
+    public virtual void OnScreenEnable()
+    {
+        this.gameObject.SetActive(true);
+    }
+    
+    public virtual void OnScreenDisable()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+}
---
+++ Assets/Code/Managers/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private List<BaseScreenController> baseScreenControllers = new List<BaseScreenController>();
+    
+    void Start()
+    {
+        foreach (var baseScreenController in baseScreenControllers)
+        {
+            baseScreenController.OnScreenDisable();
+        }
+
+        var homeScreen = FindScreen(ScreenTypes.HomeScreen);
+        homeScreen.OnScreenEnable();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    
+    
+    // HELPER FUNCTIONS
+
+    BaseScreenController FindScreen(ScreenTypes type)
+    {
+        foreach (var baseScreenController in baseScreenControllers)
+        {
+            if (baseScreenController.GetScreenType() == type)
+            {
+                return baseScreenController;
+            }
+        }
+
+        Debug.Log("No base screen type found");
+        return baseScreenControllers[0];
+    }
+}
