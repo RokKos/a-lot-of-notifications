6cd83c7ad0f674561497c39636e2710085b494aa
RokKos <rocki5555@gmail.com>
2020-03-15 13:14:14 +0100

    Spamming notifications draft

---
+++ Assets/Code/Controllers/EmailSortingMinigame/NotificationSpawnerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NotificationSpawnerController : MonoBehaviour
+{
+    [SerializeField] private NotificationController prefabNotificationController = null;
+    
+    [SerializeField] private Transform parentNotifications = null;
+
+
+    private int numNotifications = 0;
+    private void Start()
+    {
+        numNotifications = 0;
+    }
+
+    public void SpawnNotification()
+    {
+        NotificationController notificationController = Instantiate(prefabNotificationController, parentNotifications);
+        notificationController.Init(numNotifications);
+
+        numNotifications++;
+    }
+
+    public void DeleteNotification()
+    {
+        
+        numNotifications--;
+    }
+}
