ed3b55c700b07f24b1095d05c559d286561014ae
RokKos <rocki5555@gmail.com>
2020-03-15 23:22:36 +0100

    Forgot to push ProgressBarcontroller

---
+++ Assets/Code/Controllers/HomeScreen/ProgressBarController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProgressBarController : MonoBehaviour
+{
+    [SerializeField] private RectTransform rectTransform;
+    
+    [SerializeField] private float minBarValue = 0.0f;
+    [SerializeField] private float maxBarValue = 330.0f;
+
+    
+    
+    public void SetBarProcent(float procent)
+    {
+        float barValue = Mathf.Lerp(maxBarValue, minBarValue, procent);
+        rectTransform.offsetMax = new Vector2(-barValue, 0); 
+    }
+}
