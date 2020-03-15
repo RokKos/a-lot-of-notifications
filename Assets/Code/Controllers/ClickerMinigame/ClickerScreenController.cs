a486949355c033112e4ce35e859750e3561ea446
Miha Frangež <miha.frangez@gmail.com>
2020-03-15 19:52:36 +0100

    Clicker game - most of visuals done

--- Assets/Code/Controllers/ClickerMinigame/ClickerScreenController.cs
+++ Assets/Code/Controllers/ClickerMinigame/ClickerScreenController.cs
@@ -1,15 +1,82 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ClickerScreenController : BaseScreenController
 {
+    const int INITIAL_POST_NUM = 10;
+
+    [Header("Values to randomly pick from")]
+    public List<Sprite> images;
+    public List<Sprite> avatars;
+    public List<string> usernames;
+    public List<string> texts;
+
+    GameObject scrollViewContent;
+
+    public static ClickerScreenController instance;
+
+    int lastPost = 0;
+    List<IGPost> posts;
+
+    public override void OnScreenEnter()
+    {
+        base.OnScreenEnter();
+
+        // Find references
+        scrollViewContent = transform.Find("Scroll View/Viewport/Content").gameObject;
+        IGPost.postPrefab = scrollViewContent.transform.Find("tpl_ig_post").gameObject;
+
+        // Hide post template
+        IGPost.postPrefab.SetActive(false);
+
+        /*
+         * Seed initial posts
+         */
+        posts = new List<IGPost>();
+        
+        for (lastPost = 0; lastPost < INITIAL_POST_NUM; lastPost++)
+        {
+            IGPost post = makeRandomPost(lastPost);
+            posts.Add(post);
+            attachPost(post);
+        }
+
+    }
+
     public override void OnScreenUpdate()
     {
         base.OnScreenUpdate();
-        
-        // BOLT
-        // OTHER STUFF
-        
+        // TODO: Actual game code!
+    }
+
+    void attachPost(IGPost post)
+    {
+        post.gameObject.transform.SetParent(scrollViewContent.transform, false);
+        scrollViewContent.GetComponent<RectTransform>().sizeDelta = scrollViewContent.GetComponent<RectTransform>().sizeDelta + new Vector2(0, IGPost.HEIGHT);
+    }
+
+    public override void OnScreenExit()
+    {
+        base.OnScreenExit();
+
+        // Clean up all posts
+        foreach (IGPost post in posts)
+        {
+            post.destroy();
+        }
+        posts.Clear();
+    }
+
+    public IGPost makeRandomPost(int number)
+    {
+        // TODO: some less...stupid logic
+        return IGPost.make(number, pickRandom(usernames), Random.Range(0, 99999999), pickRandom(texts), pickRandom(images), pickRandom(avatars));
+    }
+
+    // TODO: maybe move utils somewhere else?
+    static T pickRandom<T>(List<T> list)
+    {
+        return list[Random.Range(0, list.Count)];
     }
 }
---
+++ Assets/Code/Controllers/ClickerMinigame/IGPost.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[ExecuteInEditMode]
+public class IGPost : MonoBehaviour
+{
+    public const int HEIGHT = 700;
+
+    public string Username;
+    public int Likes;
+    public string Text;
+    public Sprite Image;
+    public Sprite Avatar;
+
+    [Tooltip("Position/index in the list")]
+    public int Number;
+
+    public static GameObject postPrefab;
+    
+    public static IGPost make(int number, string username, int likes, string text, Sprite image, Sprite avatar)
+    {
+        GameObject go = Instantiate(postPrefab, new Vector3(0, 0, 0), Quaternion.identity);
+        IGPost post = go.GetComponent<IGPost>();
+
+        post.Number = number;
+        post.Username = username;
+        post.Likes = likes;
+        post.Text = text;
+        post.Image = image;
+        post.Avatar = avatar;
+
+        // We're cloning from a hidden template, so make yourself visible
+        go.SetActive(true);
+
+        // Update values
+        post.update();
+
+        return post;
+        
+    }
+
+    public void update()
+    {
+        gameObject.GetComponent<RectTransform>().anchoredPosition = new Vector3(0, -this.Number * HEIGHT, 0);
+
+        transform.Find("Image").GetComponent<Image>().sprite = Image;
+        transform.Find("Avatar image").GetComponent<Image>().sprite = Avatar;
+        transform.Find("Like number").GetComponent<Text>().text = String.Format("{0:n0}", Likes);
+        transform.Find("Username").GetComponent<Text>().text = Username;
+        transform.Find("Text").GetComponent<Text>().text =  "<b>" + Username + "</b> " + Text;
+    }
+
+    public override string ToString()
+    {
+        return "[IGPost " + Number + " by " + Username + "]";
+    }
+
+    public void likeClicked()
+    {
+        Debug.Log("Like clicked on post " + this);
+        // TODO: actual gameplay??
+    }
+
+    internal void destroy()
+    {
+        Destroy(this.gameObject);
+    }
+
+    // Makes it work in the editor. No touch!
+    void OnValidate()
+    {
+        update();
+    }
+}
