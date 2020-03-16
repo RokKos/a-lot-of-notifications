e7cc58a1f35bdd971cb37c3a109490ad82472a76
Brin <brin@zvan.cc>
2020-03-16 19:34:23 +0100

    Mouse feedback.

---
+++ Assets/feedbackOnMouse.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+
+public class feedbackOnMouse : MonoBehaviour
+{
+    [SerializeField] Camera camera;
+    [SerializeField] TextMeshProUGUI feedbackText;
+ 
+    void Update()
+    {
+        var mousePosition = Input.mousePosition;
+        var mousWorldPos = camera.ScreenToWorldPoint(mousePosition);
+
+        transform.position = mousePosition;
+    }
+
+    public void PunchPositiveFeedback(string feedbackMsg)
+    {
+        feedbackText.text = feedbackMsg;
+        transform.DOPunchScale(Vector3.one, 0.2f, 4, 1);
+    }
+
+
+}
